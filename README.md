69db0e0bc0d7cdf4814050b28060ca41a67598d2
Johan Cornelissen <johan1252@gmail.com>
2017-12-26 16:30:52 -0500

    Testing wit.ai api

--- README.md
+++ README.md
@@ -6,3 +6,28 @@ https://developers.facebook.com/apps
 
 FB App Token:
 EAAidYoZBZCnusBAJZB0P8sCZAes4TZBfUOdQkEvtqbTUZAssSukLpccaqTPUqbkiWQwCQBMLMoVNBZARSC6E83ZCSZABlJRN5F3sx292LqgK3W5p996Cvi2JfN7a7A7XK1kEpZBRU59gZBFqFcFZBd3MiR6nJarCSd7gEm0pL9URusQadgZDZD
+
+Command for enabling page with app:
+curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAidYoZBZCnusBAJZB0P8sCZAes4TZBfUOdQkEvtqbTUZAssSukLpccaqTPUqbkiWQwCQBMLMoVNBZARSC6E83ZCSZABlJRN5F3sx292LqgK3W5p996Cvi2JfN7a7A7XK1kEpZBRU59gZBFqFcFZBd3MiR6nJarCSd7gEm0pL9URusQadgZDZD"
+
+Setting env variable in heroku:
+heroku config:set FB_PAGE_ACCESS_TOKEN=EAAidYoZBZCnusBAJZB0P8sCZAes4TZBfUOdQkEvtqbTUZAssSukLpccaqTPUqbkiWQwCQBMLMoVNBZARSC6E83ZCSZABlJRN5F3sx292LqgK3W5p996Cvi2JfN7a7A7XK1kEpZBRU59gZBFqFcFZBd3MiR6nJarCSd7gEm0pL9URusQadgZDZD
+
+
+Change Greeting:
+curl -X POST -H "Content-Type: application/json" -d '{
+  "greeting": [
+    {
+      "locale":"default",
+      "text":"Hi, Welcome to Johan Cornelissen'\''s personal chat bot. Type a phrase like \"What can you tell me about Johan?\" to get started." 
+    }, {
+      "locale":"en_US",
+      "text":"Hi, Welcome to Johan Cornelissen'\''s personal chat bot. Type a phrase like \"What can you tell me about Johan?\" to get started."
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAidYoZBZCnusBAJZB0P8sCZAes4TZBfUOdQkEvtqbTUZAssSukLpccaqTPUqbkiWQwCQBMLMoVNBZARSC6E83ZCSZABlJRN5F3sx292LqgK3W5p996Cvi2JfN7a7A7XK1kEpZBRU59gZBFqFcFZBd3MiR6nJarCSd7gEm0pL9URusQadgZDZD"
+
+Get Started Button:
+curl -X POST -H "Content-Type: application/json" -d '{
+  "get_started": {"payload": "test payload"}
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAidYoZBZCnusBAJZB0P8sCZAes4TZBfUOdQkEvtqbTUZAssSukLpccaqTPUqbkiWQwCQBMLMoVNBZARSC6E83ZCSZABlJRN5F3sx292LqgK3W5p996Cvi2JfN7a7A7XK1kEpZBRU59gZBFqFcFZBd3MiR6nJarCSd7gEm0pL9URusQadgZDZD"
--- index.js
+++ index.js
@@ -7,6 +7,83 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+// Wit.ai parameters
+const WIT_TOKEN = process.env.WIT_TOKEN;
+
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+// Our bot actions
+const actions = {
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return sendTextMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+};
+
+
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  actions,
+  logger: new log.Logger(log.INFO)
+});
+
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -39,7 +116,8 @@ app.post('/webhook/', function (req, res) {
         }
         if (event.referral) {
     	    let text = JSON.stringify(event.referral)
-    	    sendTextMessage(sender, "Referral received: "+text.substring(0, 200), token)
+    	    sendTextMessage(sender, "Hi! I am Johan's personal chatbot, please ask me any questions related to Johan's personal experiences.")
+			sendTextMessage(sender, "Type a phrase like \"What can you tell me about Johan?\" to get started.")
     	    continue
         }
       if (event.message && event.message.text) {
@@ -53,9 +131,54 @@ app.post('/webhook/', function (req, res) {
 			continue
 		}
   	    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		
+          // We retrieve the user's current session, or create one if it doesn't exist
+          // This is needed for our bot to figure out the conversation history
+          const sessionId = findOrCreateSession(sender);
+
+          // We retrieve the message content
+          const {text, attachments} = event.message;
+
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message
+            sendTextMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+              sessionId, // the user's current session
+              text, // the user's message
+              sessions[sessionId].context // the user's current session state
+            ).then((context) => {
+              // Our bot did everything it has to do.
+              // Now it's waiting for further messages to proceed.
+              console.log('Waiting for next user messages');
+
+              // Based on the session state, you might want to reset the session.
+              // This depends heavily on the business logic of your bot.
+              // Example:
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
+              // Updating the user's current session state
+              sessions[sessionId].context = context;
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
+          }
+		
+		
       }
 
     }
+	
+	//Send status saying we received okay.
     res.sendStatus(200)
   })
 
