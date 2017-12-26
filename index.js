5b25b53b4a0637ee894d2c66ee719da7de1a8dff
Johan Cornelissen <johan1252@gmail.com>
2017-12-26 11:18:11 -0500

    Add debug print

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
+	print(messaging_events)
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
